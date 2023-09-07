c9507ced56e06045edeb5b5a70909de673a37225
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2023-09-07 22:44:16 +0800

    added the button but removed the browser router

--- src/Page2.js
+++ src/Page2.js
@@ -1,19 +1,40 @@
-import page2image1 from "./Images/page2image1.jpg"; 
-import page2image2 from "./Images/page2image2.jpg"; 
-import page2image3 from "./Images/page2image3.jpg"; 
-import page2image4 from "./Images/page2image4.jpg"; 
-import fireChat from "./Images/firechat.png"; 
- 
- 
-export default function Page2() {    
-    return( 
-        <div style={{flexDirection: "column", alignItems: "center", display: "flex"}}> 
-            <p style={{fontSize: '60'}}>Big Dick</p>
-            <img src = {fireChat} alt = "fireChat" style={{width: "70px", zIndex: '1', position:'absolute', top: 30}}/> 
-            <img src={page2image1} alt = "image1" style={{width: "50%", flex:1}}/> 
-            <img src={page2image2} alt = "image2" style={{width: "50%", flex:1}}/> 
-            <img src={page2image3} alt = "image3" style={{width: "50%", flex:1}}/> 
-            <img src={page2image4} alt = "image4" style={{width: "50%", flex:1}}/> 
-        </div> 
-    ) 
+import page2image1 from "./Images/page2image1.jpg";
+import page2image2 from "./Images/page2image2.jpg";
+import page2image3 from "./Images/page2image3.jpg";
+import page2image4 from "./Images/page2image4.jpg";
+import fireChat from "./Images/firechat.png";
+import darkerfirechat from "./Images/darkerfirechat.png";
+import { useState } from "react";
+
+export default function Page2() {
+  const [pressed, setpressed] = useState(1); //to toggle between the pressed and not pressed button for fire chat
+  const handleClick = () => {
+    console.log("firechat");
+    setpressed(pressed ^ 1);
+  };
+  return (
+    <div
+      style={{ flexDirection: "column", alignItems: "center", display: "flex" }}
+    >
+      {pressed ? (
+        <img
+          onClick={handleClick}
+          src={fireChat}
+          alt="fireChat"
+          style={{ width: "70px", zIndex: "1", position: "absolute" }}
+        />
+      ) : (
+        <img
+          onClick={handleClick}
+          src={darkerfirechat}
+          alt="darkerfireChat"
+          style={{ width: "70px", zIndex: "1", position: "absolute" }}
+        />
+      )}
+      <img src={page2image1} alt="image1" style={{ width: "50%", flex: 1 }} />
+      <img src={page2image2} alt="image2" style={{ width: "50%", flex: 1 }} />
+      <img src={page2image3} alt="image3" style={{ width: "50%", flex: 1 }} />
+      <img src={page2image4} alt="image4" style={{ width: "50%", flex: 1 }} />
+    </div>
+  );
 }
